54874908e691983a0183ecab85469b3bc43ada05
Huraira <muhammadhuraira4@gmail.com>
2022-03-26 11:25:36 +0500

    Lab4 first commit

---
+++ q1/lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DiceRoll());
+}
+
+class DiceRoll extends StatefulWidget {
+  const DiceRoll({Key? key}) : super(key: key);
+
+  @override
+  State<DiceRoll> createState() => _DiceRollState();
+}
+
+class _DiceRollState extends State<DiceRoll> {
+  int myDice = Random().nextInt(6) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.blue,
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                      margin: EdgeInsets.all(100),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            myDice = Random().nextInt(6) + 1;
+                          });
+                        },
+                        child:
+                            Image(image: AssetImage('images/dice$myDice.png')),
+                      )),
+                ),
+                Container(
+                  width: double.infinity,
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
